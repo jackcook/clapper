57d40701a64648b0e8dc8808a7c8020c6165f650
Claire Glendening <glendenc@bxscience.edu>
2016-04-17 07:24:47 +0000

    moved stuff around

--- lol.py
+++ lol.py
@@ -1,9 +1,10 @@
-import pyupm_mic as upmMicrophone
+import pyupm_mic as microphone
 import requests
 import time
 
-myMic = upmMicrophone.Microphone(0)
-threshContext = upmMicrophone.thresholdContext()
+mic = microphone.Microphone(0)
+
+threshContext = microphone.thresholdContext()
 threshContext.averageReading = 0
 threshContext.runningAverage = 0
 threshContext.averagedOver = 2
@@ -13,27 +14,36 @@ average = 0
 clap = 0
 last = 0
 
-while(1):
-  buffer = upmMicrophone.uint16Array(128)
-  length = myMic.getSampledWindow(2, 128, buffer)
-  if length:
-    thresh = myMic.findThreshold(threshContext, 30, buffer, length)
-    if thresh:
-      if average > 0 and thresh > average * 1.1 and thresh > last:
-        clap += 1
-        print "claps: %d" % clap
-      values.append(thresh)
-      if len(values) >= 3:
-        if values[-1] == values[-2] == values[-3]:
-          if clap > 0:
-            try:
-              requests.get("http://172.20.10.1:12345/clap?n=%d" % clap)
-            except:
-              print "host is down"
-          print "threshold established: %d" % values[-1]
-          average = values[-1]
-          clap = 0
-    last = thresh
+def read_sound_sensor():
+    global average, clap, last, values
 
-del myMic
+    buffer = microphone.uint16Array(128)
+    length = mic.getSampledWindow(2, 128, buffer)
+    if length:
+        thresh = mic.findThreshold(threshContext, 30, buffer, length)
+        if thresh:
+            if average > 0 and thresh > average * 1.1 and thresh > last:
+                clap += 1
+                print "claps: %d" % clap
+            values.append(thresh)
+            if len(values) >= 3:
+                if values[-1] == values[-2] == values[-3]:
+                    if clap > 0:
+                        try:
+                            requests.get("http://172.20.10.1:12345/clap?n=%d" % clap)
+                        except:
+                            print "host is down"
+                    print "threshold established: %d" % values[-1]
+                    average = values[-1]
+                    clap = 0
+        last = thresh
+
+def read_light_sensor():
+    pass
+
+while 1:
+    read_sound_sensor()
+    read_light_sensor()
+
+del mic
 
