dcf1918bde5b6111abad94c18077bc59db1835c8
Claire Glendening <glendenc@bxscience.edu>
2016-04-17 09:27:33 +0000

    Fixing the light sensor

--- lol.py
+++ lol.py
@@ -2,11 +2,9 @@ import pyupm_mic as microphone
 import requests
 import time
 import pyupm_grove as grove
-import mraa
-import sys
-import pyupm_grove
 
 mic = microphone.Microphone(0)
+light = grove.GroveLight(3)
 
 threshContext = microphone.thresholdContext()
 threshContext.averageReading = 0
@@ -41,58 +39,16 @@ def read_sound_sensor():
                     average = values[-1]
                     clap = 0
         last = thresh
-"""
+
 def read_light_sensor():
-    print "light"
+    try:
+        requests.get("http://172.20.10.1:12345/light?n=%d" % clap)
+    except:
+        print "host is down"
+
+    print "%s raw value is %d lux" % (light.name(), light.value())
 
 while 1:
     read_sound_sensor()
     read_light_sensor()
 
-del mic
-
-light=grove.groveLight(3)
-
-while 1:
-    print light.name() + "raw value is %d" % light.raw_value()+\
-        ", which is roughly %d" %light.value() + " lux";
-    time.sleep(3)
-
-del light"""
-
-
-LIGHT_SENSOR_PIN=3
-MAX_LIGHT = 50
-LED_PWM_PIN=5
-
-def main(): 
-    light = pyupm_grove.GroveLight(LIGHT_SENSOR_PIN)
-    pwm = mraa.Pwm(LED_PWM_PIN)
-    pwm.period_us(5000) # Set the period as 5000 us or 5ms
-    pwm.enable(True)    # enable PWM
-    pwm.write(0)    
-    print "Light sensor bar:"
-    while True:
-        ambientLight = light.value()
-        sys.stdout.write("Light sensor: %02d " %ambientLight)
-        sys.stdout.write("[")
-        tempLight = ambientLight
-        if tempLight > MAX_LIGHT:
-            tempLight = MAX_LIGHT      # Nromalize the value
-            
-        pwmValue = (MAX_LIGHT - tempLight)/float(MAX_LIGHT)
-
-        pwm.write(pwmValue)
-        
-        for i in range(0, MAX_LIGHT):
-            if ambientLight > i:
-                sys.stdout.write("=")
-            elif ambientLight == i:
-                sys.stdout.write("|")
-            else:
-                sys.stdout.write(" ")
-            sys.stdout.write("]  \r")
-        sys.stdout.flush()
-        time.sleep(0.1)
-if __name__ == "__main__":
-    main()
